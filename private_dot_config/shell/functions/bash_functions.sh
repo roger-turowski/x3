6f675def76c70c0e8bcfc715011cbc20faf3b019
Roger Turowski <roger.turowski@protonmail.com>
2026-07-06 03:15:11 -0400

    Add bash_functions.sh

---
+++ private_dot_config/shell/functions/bash_functions.sh
@@ -0,0 +1,12 @@
+is_file_exists() {
+    local f="$1"
+    if [[ -f "$f" ]]; then
+        return 0  # Success
+    else
+        return 1  # Failure
+    fi
+}
+
+chezmoi-cd() {
+  cd "$(chezmoi source-path "$@")"
+}   
