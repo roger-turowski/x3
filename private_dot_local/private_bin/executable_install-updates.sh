c6e73e87276a275b7f62e764cb47d89c89d39df6
Roger Turowski <roger.turowski@protonmail.com>
2026-09-04 11:14:07 -0400

    Added total redirection to install-updates.sk logs

--- private_dot_local/private_bin/executable_install-updates.sh
+++ private_dot_local/private_bin/executable_install-updates.sh
@@ -10,13 +10,13 @@ source /etc/os-release
 
 case "$ID" in
     opensuse-tumbleweed)
-	time sudo zypper ref | tee -a "${logfile}" && \
-	time sudo zypper --non-interactive dup --auto-agree-with-licenses --allow-vendor-change | tee -a "${logfile}"
+	time sudo zypper ref 2>&1 | tee -a "${logfile}" && \
+	time sudo zypper --non-interactive dup --auto-agree-with-licenses --allow-vendor-change 2>&1 | tee -a "${logfile}"
 	;;
     arch)
-	time sudo pacman -Syu | tee -a "${logfile}"
+	time sudo pacman -Syu --noconfirm 2>&1 | tee -a "${logfile}"
 	;;
     *)
-	echo "OS not implemented yet: ${ID}"
+	echo "OS not implemented yet: ${ID}" 2>&1 | tee -a "${logfile}" 
 	;;
 esac
